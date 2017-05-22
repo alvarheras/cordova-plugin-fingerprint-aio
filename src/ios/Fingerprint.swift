23262ddc19645fcbbaa7b70636b0dac0495a2733
alvarheras <alvarheras@gmail.com>
2017-05-22 11:02:22 +0200

    COMPILE TO PHONEGAP BUILD

--- src/ios/Fingerprint.swift
+++ src/ios/Fingerprint.swift
@@ -2,45 +2,49 @@ import Foundation
 import LocalAuthentication
 
 @objc(Fingerprint) class Fingerprint : CDVPlugin {
-
-  func isAvailable(command: CDVInvokedUrlCommand){
-    let authenticationContext = LAContext();
-    var error:NSError?;
-
-    let available = authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error);
-
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Not available");
-    if available == true {
-      pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Available");
-    }
-
-    commandDelegate.send(pluginResult, callbackId:command.callbackId);
-  }
-
-  func authenticate(command: CDVInvokedUrlCommand){
-    let authenticationContext = LAContext();
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something went wrong");
-    var reason = "Authentication";
-    let data  = command.arguments[0] as AnyObject?;
-
-    if let clientId = data?["clientId"] as! String? {
-      reason = clientId;
-    }
-
-
-    authenticationContext.evaluatePolicy(
-      .deviceOwnerAuthenticationWithBiometrics,
-      localizedReason: reason,
-      reply: { [unowned self] (success, error) -> Void in
-        if( success ) {
-          pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Success");
-        }else {
-          // Check if there is an error
-          if error != nil {
-            pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Error: \(error)");
-          }
+    
+    func isAvailable(command: CDVInvokedUrlCommand) {
+        let authenticationContext = LAContext();
+        var error:NSError?;
+        
+        let available = authenticationContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error);
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Not available");
+        if available == true {
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Available");
         }
-        self.commandDelegate.send(pluginResult, callbackId:command.callbackId);
-      });
+        
+        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId);
     }
-  }
+    
+    func authenticate(command: CDVInvokedUrlCommand) {
+        
+        let authenticationContext = LAContext();
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Something went wrong");
+        var reason = "Authentication";
+        let data  = command.arguments[0] as AnyObject?;
+        
+        if let clientId = data?["clientId"] as! String? {
+            reason = clientId;
+        }
+        
+        
+        authenticationContext.evaluatePolicy(
+            .DeviceOwnerAuthenticationWithBiometrics,
+            localizedReason: reason,
+            reply: { [unowned self] (success, error) -> Void in
+                if( success ) {
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Success");
+                }else {
+                    // Check if there is an error
+                    if error != nil {
+                        pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Error: \(error)");
+                    }
+                }
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId);
+                
+            });
+        
+    } 
+    
+}
